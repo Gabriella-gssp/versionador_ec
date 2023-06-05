d5d96080ffff42813a9e201bd312669fd1452b22
Gabriella Pereira <gabriellapereiracoro@gmail.com>
2023-06-05 11:14:18 -0300

    codigo.h comando iniciar

---
+++ versionador/versionador.h
@@ -0,0 +1,12 @@
+#ifndef VERSIONADOR_H_
+#define VERSIONADOR_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+void criarDiretorios();
+
+#endif 
