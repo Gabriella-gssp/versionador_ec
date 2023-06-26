8138fdee0c5242e3d2ffdaecb4d2893df969f4eb
Gabriella Pereira <134066551+Gabriella-gssp@users.noreply.github.com>
2023-06-25 23:35:10 -0300

    inserçao das structs

--- versionador/versionador.h
+++ versionador/versionador.h
@@ -1,6 +1,26 @@
 #ifndef VERSIONADOR_H_
 #define VERSIONADOR_H_
 
+typedef struct{
+    char arquivo[256];
+    struct Node* prox;
+} Node;
+
+typedef struct {
+    Node* inicio;
+    Node* fim;
+} Fila;
+
+typedef struct {
+    int identificador;
+    Fila* arquivos;
+    struct Snapshot* prox;
+} Snapshot;
+
+void criarDiretorios();
+
+#endif 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
